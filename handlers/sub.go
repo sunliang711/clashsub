98a23bddfc2b37413fbfdac28fde971957895aa5
sunliang <sunliang711@163.com>
2021-02-11 17:04:17 +0800

    support multiple config

--- handlers/sub.go
+++ handlers/sub.go
@@ -23,10 +23,13 @@ func InitTemplate() {
 }
 
 type serverConfig struct {
+	Name     string
+	Type     string
 	Server   string
 	Port     string
 	Cipher   string
 	Password string
+	UDP      bool
 }
 
 func Sub(ctx *gin.Context) {
@@ -38,21 +41,35 @@ func Sub(ctx *gin.Context) {
 		return
 	}
 
-	if viper.GetString("configs."+port+".server") == "" {
-		msg := fmt.Sprintf("no such port: %v", port)
+	// if viper.GetString("configs."+port) == "" {
+	// 	msg := fmt.Sprintf("no such port: %v", port)
+	// 	logrus.Errorf(msg)
+	// 	ctx.String(200, msg)
+	// 	return
+	// }
+
+	var serverConfigs []serverConfig
+	err := viper.UnmarshalKey("configs."+port, &serverConfigs)
+	if err != nil {
+		msg := fmt.Sprintf("Unmarshal configs.%v error: %v", port, err)
 		logrus.Errorf(msg)
 		ctx.String(200, msg)
 		return
 	}
-
-	cfg := &serverConfig{
-		Server:   viper.GetString("configs." + port + ".server"),
-		Port:     port,
-		Cipher:   viper.GetString("configs." + port + ".cipher"),
-		Password: viper.GetString("configs." + port + ".password"),
+	if len(serverConfigs) == 0 {
+		msg := fmt.Sprintf("No config with: %v", port)
+		logrus.Warnf(msg)
+		ctx.String(200, msg)
+		return
 	}
+	// cfg := &serverConfig{
+	// 	Server:   viper.GetString("configs." + port + ".server"),
+	// 	Port:     port,
+	// 	Cipher:   viper.GetString("configs." + port + ".cipher"),
+	// 	Password: viper.GetString("configs." + port + ".password"),
+	// }
 	var buf bytes.Buffer
-	err := tmpl.Execute(&buf, cfg)
+	err = tmpl.Execute(&buf, serverConfigs)
 	if err != nil {
 		msg := fmt.Sprintf("execute template error: %v", err)
 		logrus.Error(msg)
